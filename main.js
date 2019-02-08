2d48cfb23210f1d4c0c34aeb811fd71491144680
stonengineer <josephwstone@gmail.com>
2019-02-08 16:16:14 -0600

    oauth2 login/logout work, trying to figure out tooling api

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const electron = require('electron');
 const path = require('path');
 const url = require('url');
 const fs = require('fs');
+const jsforce = require('jsforce');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const ipcMain = electron.ipcMain;
@@ -10,20 +11,44 @@ const WIDTH = 800;
 const HEIGHT = 600;
 
 let currWindow;
-let connection;
+let sfconn;
 
 function start() {
     currWindow = createWindowAt('login.html');
-    currWindow.webContents.openDevTools();
 }
 
-app.on('login', () => {
+ipcMain.on('login-successful', (event, conn) => {
+    sfconn = conn;
+    console.log(sfconn);
     currWindow = createWindowAt('main.html');
-})
+});
 
-ipcMain.on('login-successful', (conn) => {
-    connection = conn;
-    currWindow = createWindowAt('main.html');
+ipcMain.on('run-apex', (event, code) => {
+    sfconn.tooling = new jsforce.Tooling({
+        conn: sfconn
+    });
+    console.log(sfconn);
+    sfconn.tooling.executeAnonymous(code, function(err, res) {
+        if (err) { return console.error(err); }
+        console.log("compiled?: " + res.compiled); // compiled successfully
+        console.log("executed?: " + res.success); // executed successfully
+        console.log('response: '+JSON.stringify(res));
+    });
+});
+
+ipcMain.on('logout-pressed', () => {
+    var conn = new jsforce.Connection({
+        sessionId: sfconn.accessToken,
+        serverURL: sfconn.instanceURL
+    });
+    conn.logoutByOAuth2(function(error) {
+        if(error) {
+            console.log(error);
+        } else {
+            console.log('logout successful');
+        }
+    });
+    currWindow = createWindowAt('login.html');
 });
 
 function createWindowAt(htmlFile) {
